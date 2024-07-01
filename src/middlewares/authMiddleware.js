5c4459419925707e94a4708d8ae1e24424ba6973
phducdung04 <phamducdung147@gmail.com>
2024-07-01 16:15:49 +0700

    pull

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,23 +3,25 @@ require("dotenv").config();
 
 
 
-module.exports ={ veryfyUser  :async (req, res, next) => {
-  const accessToken = req.cookies.accesstoken ;
-  
-  if (accessToken) {
-    try {
-      const user = jwt.verify(
-        accessToken,
-        process.env.ACCESS_TOKEN_SECRET
-      );
-      const userRoles = user.roles;
-      req.roles = userRoles;
-      // return res.redirect("/admin/editLandingPage");
-      next();
-    } catch (error) {
+module.exports = {
+  veryfyUser: async (req, res, next) => {
+    const accessToken = req.cookies.accesstoken;
+
+    if (accessToken) {
+      try {
+        const user = jwt.verify(
+          accessToken,
+          process.env.ACCESS_TOKEN_SECRET
+        );
+        const userRoles = user.roles;
+        req.roles = userRoles;
+        // return res.redirect("/admin/editLandingPage");
+        next();
+      } catch (error) {
+        return res.redirect("/admin");
+      }
+    } else {
       return res.redirect("/admin");
     }
-  } else {
-    return res.redirect("/admin");
   }
-}};
+};
